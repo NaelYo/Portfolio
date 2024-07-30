1be530557beb8529b6453e9298954e91e216a3be
NaelYo <naeltampan@gmail.com>
2024-07-30 10:51:48 +0700

    first-commit

---
+++ tailwind.config.js
@@ -0,0 +1,84 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#f59e0b',
+        dark: '#0f172a',
+        secondary: '#64748b'
+      },
+      screens: {
+        '2xl': '1280px'
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-45deg)' },
+          '50%': { transform: 'rotate(45deg)' }
+        },
+        fadeRight: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(-2rem)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateX(0)'
+          }
+        },
+        fadeLeft: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(2rem)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateX(0)'
+          }
+        },
+        fadeUp: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(2rem)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          }
+        },
+        wiggleUp: {
+          '0%, 100%': {
+            transform: 'translateY(0)'
+          },
+          '50%': {
+            transform: 'translateY(-5%)'
+          },
+        },
+      },
+      animation: {
+        'wiggle-1.5s': 'wiggle 1500ms ease-in-out infinite',
+        'wiggle-2s': 'wiggle 2000ms ease-in-out infinite',
+        'wiggle-2.5s': 'wiggle 2500ms ease-in-out infinite',
+        'fadeRight-500': 'fadeRight 500ms ease-in-out',
+        'fadeRight-800': 'fadeRight 800ms ease-in-out',
+        'fadeRight-1.1': 'fadeRight 1100ms ease-in-out',
+        'fadeRight-1.5': 'fadeRight 1500ms ease-in-out',
+        'fadeLeft-500': 'fadeLeft 500ms ease-in-out',
+        'fadeLeft-800': 'fadeLeft 800ms ease-in-out',
+        'fadeLeft-1.1': 'fadeLeft 1100ms ease-in-out',
+        'fadeLeft-1.5': 'fadeLeft 1500ms ease-in-out',
+        'fadeUp': 'fadeUp 1000ms ease-in-out',
+        'bounce': 'bounce 2000ms ease-in-out infinite',
+        'wiggleUp': 'wiggleUp 4000ms ease-in-out infinite',
+      },
+    },
+  },
+  plugins: [
+    require('tailwindcss-animated')
+  ],
+}
+
