387b12d92917e1377a599d79d0e25ce821c2a81b
NaelYo <naeltampan@gmail.com>
2024-07-30 14:30:14 +0700

    Make Navbar

---
+++ public/js/script.js
@@ -0,0 +1,18 @@
+// Navbar Fixed 
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if( window.scrollY > fixedNav ) {
+        header.classList.add('navbar-fixed')
+    } else {
+        header.classList.remove('navbar-fixed')
+    }
+}
+
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function() {
+    navMenu.classList.toggle('hidden')
+})
