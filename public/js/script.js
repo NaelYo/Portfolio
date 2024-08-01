57e78f635550ca4bb2b3a55b4303d2ad9cf9355d
NaelYo <naeltampan@gmail.com>
2024-08-01 19:20:43 +0700

    update hero section

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,15 @@
 // Navbar Fixed 
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if( window.scrollY > fixedNav ) {
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed');
+    }
+}
+
 
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
