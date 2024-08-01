df13a2ad6c9737bb136e3f7e50473543324a47da
NaelYo <naeltampan@gmail.com>
2024-08-01 18:46:48 +0700

    update navbar

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,4 @@
 // Navbar Fixed 
-window.onscroll = function() {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
-
-    if( window.scrollY > fixedNav ) {
-        header.classList.add('navbar-fixed')
-    } else {
-        header.classList.remove('navbar-fixed')
-    }
-}
 
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
